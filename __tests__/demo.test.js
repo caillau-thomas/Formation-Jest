492ea5d46f79da9e08f25e2efcc6c994a76e294a
caillau-thomas <thomas.caillau@gmail.com>
2022-06-24 16:57:36 +0200

    premiers test

---
+++ __tests__/demo.test.js
@@ -0,0 +1,15 @@
+describe('pack demo', function(){
+    
+    
+    
+    test('Demo somme 1', function(){
+        const a = 2+ 2
+        expect(a).toBe(4)
+    })
+
+    test('Demo somme 2', function(){
+        const a = 2+ 2
+        expect(a).toBe(4)
+    })
+})
+
